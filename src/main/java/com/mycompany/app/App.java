d058e1af231bc576e0af9248f96ca34716735f43
sdunnvo <124534566+sdunnvo@users.noreply.github.com>
2023-03-20 18:18:41 -0700

    Create App.java
    simple App.

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,17 @@
+/*-------------------------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
+ *-------------------------------------------------------------------------------------------------------------*/
+
+package com.mycompany.app;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello CrashPlan World!" );
+
+    }
+
+}
